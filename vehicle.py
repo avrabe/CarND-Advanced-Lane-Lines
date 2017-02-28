e186c1088b1177623282b47b6616c67aec069d11
avrabe <ralf_beier@me.com>
2017-02-28 21:10:14 +0100

    Create method to test different parameters.
    Quick check which of the paramaters seems to work best.

--- vehicle.py
+++ vehicle.py
@@ -3,6 +3,7 @@ Based on sample code form Udactiy SDND class: Vehicle Detection and Tracking - H
 """
 import glob
 import time
+from itertools import product
 
 import numpy as np
 from skimage.feature import hog
@@ -10,8 +11,6 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.svm import LinearSVC
 
-
-# Define a function to return HOG features and visualization
 from lane.image import FileImage, Color
 
 
@@ -41,7 +40,7 @@ def extract_features(imgs, cspace=Color.RGB, orient=9,
     for file in imgs:
         # Read in each one by one
         image = FileImage(filename=file)
-        #image = mpimg.imread(file)
+        # image = mpimg.imread(file)
         # apply color conversion if other than 'RGB'
 
         feature_image = image.get_image(color=cspace).image
@@ -67,60 +66,61 @@ def extract_features(imgs, cspace=Color.RGB, orient=9,
 cars = glob.glob('train/vehicles/*/*.png')
 notcars = glob.glob('train/non-vehicles/*/*.png')
 
-
 # Reduce the sample size because HOG features are slow to compute
 # The quiz evaluator times out after 13s of CPU time
 sample_size = len(cars)
 cars = cars[0:sample_size]
 notcars = notcars[0:sample_size]
 
+
+def method_name(colorspace=Color.YCrCb, orient=9, pix_per_cell=8,
+                cell_per_block=2, hog_channel="ALL"):
+    t = time.time()
+    car_features = extract_features(cars, cspace=colorspace, orient=orient,
+                                    pix_per_cell=pix_per_cell, cell_per_block=cell_per_block,
+                                    hog_channel=hog_channel)
+    notcar_features = extract_features(notcars, cspace=colorspace, orient=orient,
+                                       pix_per_cell=pix_per_cell, cell_per_block=cell_per_block,
+                                       hog_channel=hog_channel)
+    t2 = time.time()
+    print(round(t2 - t, 2), 'Seconds to extract HOG features...')
+    # Create an array stack of feature vectors
+    X = np.vstack((car_features, notcar_features)).astype(np.float64)
+    # Fit a per-column scaler
+    X_scaler = StandardScaler().fit(X)
+    # Apply the scaler to X
+    scaled_X = X_scaler.transform(X)
+    # Define the labels vector
+    y = np.hstack((np.ones(len(car_features)), np.zeros(len(notcar_features))))
+    # Split up data into randomized training and test sets
+    rand_state = np.random.randint(0, 100)
+    X_train, X_test, y_train, y_test = train_test_split(
+        scaled_X, y, test_size=0.2, random_state=rand_state)
+    print('Using:', orient, 'orientations', pix_per_cell,
+          'pixels per cell and', cell_per_block, 'cells per block')
+    print('Feature vector length:', len(X_train[0]))
+    # Use a linear SVC
+    svc = LinearSVC()
+    # Check the training time for the SVC
+    t = time.time()
+    svc.fit(X_train, y_train)
+    t2 = time.time()
+    print(round(t2 - t, 2), 'Seconds to train SVC...')
+    # Check the score of the SVC
+
+    print("%s %s: Test Accuracy of SVC = %.2f" % (colorspace.name, str(hog_channel),round(svc.score(X_test, y_test), 4)))
+    # Check the prediction time for a single sample
+    t = time.time()
+    n_predict = 10
+    print('My SVC predicts: ', svc.predict(X_test[0:n_predict]))
+    print('For these', n_predict, 'labels: ', y_test[0:n_predict])
+    t2 = time.time()
+    print(round(t2 - t, 5), 'Seconds to predict', n_predict, 'labels with SVC')
+
+
 ### TODO: Tweak these parameters and see how the results change.
-colorspace = Color.YCrCb # 'HLS'  # Can be RGB, HSV, LUV, HLS, YUV, YCrCb
-orient = 9
-pix_per_cell = 8
-cell_per_block = 2
-hog_channel = "ALL"  # Can be 0, 1, 2, or "ALL"
-
-t = time.time()
-car_features = extract_features(cars, cspace=colorspace, orient=orient,
-                                pix_per_cell=pix_per_cell, cell_per_block=cell_per_block,
-                                hog_channel=hog_channel)
-notcar_features = extract_features(notcars, cspace=colorspace, orient=orient,
-                                   pix_per_cell=pix_per_cell, cell_per_block=cell_per_block,
-                                   hog_channel=hog_channel)
-t2 = time.time()
-print(round(t2 - t, 2), 'Seconds to extract HOG features...')
-# Create an array stack of feature vectors
-X = np.vstack((car_features, notcar_features)).astype(np.float64)
-# Fit a per-column scaler
-X_scaler = StandardScaler().fit(X)
-# Apply the scaler to X
-scaled_X = X_scaler.transform(X)
-
-# Define the labels vector
-y = np.hstack((np.ones(len(car_features)), np.zeros(len(notcar_features))))
-
-# Split up data into randomized training and test sets
-rand_state = np.random.randint(0, 100)
-X_train, X_test, y_train, y_test = train_test_split(
-    scaled_X, y, test_size=0.2, random_state=rand_state)
-
-print('Using:', orient, 'orientations', pix_per_cell,
-      'pixels per cell and', cell_per_block, 'cells per block')
-print('Feature vector length:', len(X_train[0]))
-# Use a linear SVC
-svc = LinearSVC()
-# Check the training time for the SVC
-t = time.time()
-svc.fit(X_train, y_train)
-t2 = time.time()
-print(round(t2 - t, 2), 'Seconds to train SVC...')
-# Check the score of the SVC
-print('Test Accuracy of SVC = ', round(svc.score(X_test, y_test), 4))
-# Check the prediction time for a single sample
-t = time.time()
-n_predict = 10
-print('My SVC predicts: ', svc.predict(X_test[0:n_predict]))
-print('For these', n_predict, 'labels: ', y_test[0:n_predict])
-t2 = time.time()
-print(round(t2 - t, 5), 'Seconds to predict', n_predict, 'labels with SVC')
+colorspaces = [Color.RGB, Color.HSV, Color.LUV, Color.YUV, Color.YCrCb]
+hog_channels = [0,1,2,"ALL"]
+for colorspace, hog_channel in product(colorspaces, hog_channels):
+    print("--- Run with %s %s ---" % (colorspace.name, str(hog_channel)))
+    method_name(colorspace=colorspace, hog_channel=hog_channel)
